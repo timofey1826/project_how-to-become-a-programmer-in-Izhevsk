a6607be86474fb4bff222118e0864f32302c7e7c
lagunovLev <95176950+lagunovLev@users.noreply.github.com>
2023-05-05 17:12:37 +0300

    webapp

---
+++ Web/Models/Questionnaire.cs
@@ -0,0 +1,8 @@
+public class Questionnaire
+{
+    private int _id;
+    private int _age;
+    private bool _high_education;
+    private float _education_time_per_day;
+
+}
---
+++ Web/Models/User.cs
@@ -0,0 +1,64 @@
+public class User
+{
+    private string _name;
+    private string _password;
+    private string _email;
+
+    public string Name 
+    {
+        get { return _name; }
+        set 
+        {
+            if (value == "")
+                Console.WriteLine("Error: Empty string in user name");
+            else
+                _name = value;
+        }
+    }
+
+    public string Password
+    {
+        get { return _password; }
+        set
+        {
+            if (value.Length < 8)
+            {
+                Console.WriteLine("Error: Password is too short");
+                return;
+            }
+            bool capital_letter = false;
+            bool special_symbol = false;
+
+            foreach (char i in value)
+            {
+                if (Char.IsUpper(i))
+                    capital_letter = true;
+                if (!Char.IsLetterOrDigit(i))
+                    special_symbol = true;
+            }
+
+            if (!capital_letter)
+            {
+                Console.WriteLine("Error: There are no capital letters in the password");
+                return;
+            }
+
+            if (!special_symbol)
+            {
+                Console.WriteLine("Error: There are no special symbols in the password");
+                return;
+            }
+
+            _password = value;
+        }
+    }
+
+    public string Email 
+    {
+        get { return _email; }
+        set
+        { 
+            // TODO
+        }
+    }
+}
