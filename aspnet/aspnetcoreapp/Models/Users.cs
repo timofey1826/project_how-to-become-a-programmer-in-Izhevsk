c8c4fef4c69b4aba354df10e5c0e532b76b44e22
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2023-05-05 18:41:51 +0400

    3

--- aspnet/aspnetcoreapp/Models/Users.cs
+++ aspnet/aspnetcoreapp/Models/Users.cs
@@ -4,33 +4,65 @@ public class Users{
 
     private string _surname;
 
-    private string _email, sobaka;
+    private string _email;
+    bool sobaka;
+    int sobakaIndex;
     string[] pochtavr = {"gmail.com","mail.ru","","","",};
 
     
-    public string name{
+    public string Name{
         get{ return _name;}
         set
         {
             _name = value;
         }
     }
-    public string surname{
+    public string Surname{
         get{ return _surname;}
         set
         {
             _surname = value;
         }
     }
-    public string email{
+    public string Email{
         get{ return _email;}
         set
         {
-            for(int i = 0;i < value.Length; i++)
+            if(value != null)
             {
+                for(int i = 0;i < value.Length; i++)
+                {
+                    if(value[i] == '@')
+                    {
+                    sobaka = true;
+                    sobakaIndex = i;
+                    break;
+                    }
+                }
+                
+                if(sobaka == true)
+                {
+                    if(sobakaIndex != 0)
+                    {
+                        foreach(string p in pochtavr)
+                        {
+                            if(p.Length < value.Length)
+                            {
+                                for(int i = 0;i < value.Length; i++)
+                                {
+                                    if(p == value.Substring(i,i+p.Length))
+                                    {
 
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                    _email = value;
+                }
+            
             }
-            _email = value;
         }
     }
 }
