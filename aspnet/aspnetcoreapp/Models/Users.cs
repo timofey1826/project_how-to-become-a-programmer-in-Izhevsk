5ebe62333225ff1b146944059b4e97dcfc155438
timofey1826 <105168955+timofey1826@users.noreply.github.com>
2023-05-12 18:27:04 +0400

    12.05

--- aspnet/aspnetcoreapp/Models/Users.cs
+++ aspnet/aspnetcoreapp/Models/Users.cs
@@ -1,29 +1,9 @@
 public class Users{
-
-    private string _name;
-
-    private string _surname;
-
     private string _email;
-    bool sobaka;
-    int sobakaIndex;
-    string[] pochtavr = {"gmail.com","mail.ru","","","",};
+    bool pochta = false;
+    int sobakaIndex = -2, sobakaCount = 0;
+    string[] pochtavr = {"gmail.com","mail.ru","yandex.ru","yahoo.com","outlook.com"};
 
-    
-    public string Name{
-        get{ return _name;}
-        set
-        {
-            _name = value;
-        }
-    }
-    public string Surname{
-        get{ return _surname;}
-        set
-        {
-            _surname = value;
-        }
-    }
     public string Email{
         get{ return _email;}
         set
@@ -32,36 +12,36 @@ public class Users{
             {
                 for(int i = 0;i < value.Length; i++)
                 {
-                    if(value[i] == '@')
+                    if(value[i].Equals('@'))
                     {
-                    sobaka = true;
-                    sobakaIndex = i;
-                    break;
+                        sobakaCount += i;
+
+                        if(sobakaIndex == -2)
+                        {
+                            sobakaIndex = i;
+                        }
+                        break;
                     }
                 }
-                
-                if(sobaka == true)
-                {
-                    if(sobakaIndex != 0)
-                    {
-                        foreach(string p in pochtavr)
-                        {
-                            if(p.Length < value.Length)
-                            {
-                                for(int i = 0;i < value.Length; i++)
-                                {
-                                    if(p == value.Substring(i,i+p.Length))
-                                    {
 
-                                    }
-                                }
+                if(sobakaCount == 1)
+                {
+                    foreach(string p in pochtavr)
+                    {
+                        if(p.Length < value.Length-1)
+                        {
+                            if(p == value.Substring(sobakaIndex,value.Length))
+                            {
+                                pochta = true;
+                                break;
                             }
                         }
                     }
-                    
-                    _email = value;
+                    if(pochta)
+                    {
+                        _email = value;
+                    }
                 }
-            
             }
         }
     }
